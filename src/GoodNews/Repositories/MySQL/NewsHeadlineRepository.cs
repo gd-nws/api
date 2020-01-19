e69538379ab40c48576b7bfbe8cb2365cc7a3744
Daniel Welsh <danjwelsh24@gmail.com>
2020-01-19 21:58:47 +0100

    :sparkles: adding annotation counts to headlines

--- src/GoodNews/Repositories/MySQL/NewsHeadlineRepository.cs
+++ src/GoodNews/Repositories/MySQL/NewsHeadlineRepository.cs
@@ -27,14 +27,33 @@ namespace GoodNews.Repositories.MySQL
             var sort = sentiment == HeadlineSentiment.POSITIVE ? "DESC" : "ASC";
 
             return await Db.NewsHeadlines.FromSqlRaw($@"
-                SELECT *
-			    FROM headlines h
-			    WHERE 
-			      h.predicted_class {op} 0
-                AND DATE(h.published_at) = CURDATE() - INTERVAL {dateOffset} DAY
+                SELECT h.id,
+                       h.headline,
+                       h.predicted_class,
+                       h.origin,
+                       h.link,
+                       h.semantic_value,
+                       h.published_at,
+                       h.display_image,
+                       h.created_at,
+                       h.pos,
+                       h.neg,
+                       h.neu,
+                       h.hashcode,
+                       COALESCE(a.positive_votes, 0) as positive_votes,
+                       COALESCE(a.negative_votes, 0) as negative_votes
+                FROM headlines h
+                LEFT JOIN (
+                    SELECT a.headline_id,
+                        SUM(a.positive) as positive_votes,
+                        SUM(a.negative) as negative_votes
+                    FROM annotations a
+                    GROUP BY a.headline_id
+                ) a on h.id = a.headline_id
+                WHERE h.predicted_class {op} 0
+                  AND DATE(h.published_at) = CURDATE() - INTERVAL {dateOffset} DAY
                 ORDER BY h.semantic_value {sort}
-                LIMIT {limit}
-                OFFSET {offset}
+                LIMIT {limit} OFFSET {offset}
             ").ToListAsync();
         }
 
@@ -46,14 +65,35 @@ namespace GoodNews.Repositories.MySQL
         public async Task<NewsHeadline> GetHeadline(int headlineId)
         {
             var result = await Db.NewsHeadlines.FromSqlRaw($@"
-                SELECT *
+                SELECT h.id,
+                       h.headline,
+                       h.predicted_class,
+                       h.origin,
+                       h.link,
+                       h.semantic_value,
+                       h.published_at,
+                       h.display_image,
+                       h.created_at,
+                       h.pos,
+                       h.neg,
+                       h.neu,
+                       h.hashcode,
+                       COALESCE(a.positive_votes, 0) as positive_votes,
+                       COALESCE(a.negative_votes, 0) as negative_votes
                 FROM headlines h
+                LEFT JOIN (
+                    SELECT a.headline_id,
+                        SUM(a.positive) as positive_votes,
+                        SUM(a.negative) as negative_votes
+                    FROM annotations a
+                    GROUP BY a.headline_id
+                ) a on h.id = a.headline_id
                 WHERE h.id = {headlineId}
             ").ToListAsync();
 
             return result.Count > 0 ? result.First() : null;
         }
-        
+
         /// <summary>
         /// Fetch headlines sorted by sentiment count.
         /// </summary>
@@ -71,7 +111,7 @@ namespace GoodNews.Repositories.MySQL
                 AND DATE(h.published_at) = CURDATE() - INTERVAL {dateOffset} DAY
             ").CountAsync();
         }
-        
+
         /// <summary>
         /// Search headlines for a string.
         /// </summary>
@@ -80,12 +120,34 @@ namespace GoodNews.Repositories.MySQL
         /// <param name="limit"></param>
         /// <param name="offset"></param>
         /// <returns></returns>
-        public async Task<IList<NewsHeadline>> SearchHeadlines(HeadlineSentiment sentiment, string term, int limit = 10, int offset = 10)
+        public async Task<IList<NewsHeadline>> SearchHeadlines(HeadlineSentiment sentiment, string term, int limit = 10,
+            int offset = 10)
         {
             var sort = sentiment == HeadlineSentiment.POSITIVE ? "DESC" : "ASC";
             return await Db.NewsHeadlines.FromSqlRaw($@"
-                SELECT *
+                SELECT h.id,
+                       h.headline,
+                       h.predicted_class,
+                       h.origin,
+                       h.link,
+                       h.semantic_value,
+                       h.published_at,
+                       h.display_image,
+                       h.created_at,
+                       h.pos,
+                       h.neg,
+                       h.neu,
+                       h.hashcode,
+                       COALESCE(a.positive_votes, 0) as positive_votes,
+                       COALESCE(a.negative_votes, 0) as negative_votes
                 FROM headlines h
+                LEFT JOIN (
+                    SELECT a.headline_id,
+                        SUM(a.positive) as positive_votes,
+                        SUM(a.negative) as negative_votes
+                    FROM annotations a
+                    GROUP BY a.headline_id
+                ) a on h.id = a.headline_id
                 WHERE
                     h.headline LIKE '%{term}%'
                 ORDER BY h.semantic_value {sort}
@@ -93,7 +155,7 @@ namespace GoodNews.Repositories.MySQL
                 
             ").ToListAsync();
         }
-        
+
         /// <summary>
         /// Get a count for a search query
         /// </summary>
